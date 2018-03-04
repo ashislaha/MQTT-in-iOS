ebf0c68b28ff99eeb70f7ba173a69cf84ce09237
Ashis Laha <ashis.laha@olacabs.com>
2018-03-04 10:46:45 +0530

    added CocoaMQTT framework

--- MQTT/MQTT/ViewController.swift
+++ MQTT/MQTT/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import CocoaMQTT
 
 class ViewController: UIViewController {
 
+    private var mqtt: CocoaMQTT!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        createMQTTConnection()
+        
+        mqtt.publish("new topic", withString: "hi")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func createMQTTConnection() {
+        let clientID = "CocoaMQTT-" + String(ProcessInfo().processIdentifier)
+        mqtt = CocoaMQTT(clientID: clientID, host: "localhost", port: 1883)
+        mqtt.username = "test"
+        mqtt.password = "public"
+        mqtt.willMessage = CocoaMQTTWill(topic: "/will", message: "dieout")
+        mqtt.keepAlive = 60
+        mqtt.delegate = self
+        mqtt.allowUntrustCACertificate = true
+        mqtt.connect()
     }
+}
 
-
+extension ViewController: CocoaMQTTDelegate {
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        print("mqttDidPing")
+    }
+    
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        print("mqttDidReceivePong")
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        print("didPublishAck : \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishComplete id: UInt16) {
+        print("didPublishComplete: \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+        print("didSubscribeTopic: \(topic)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        print("didUnsubscribeTopic: \(topic)")
+    }
+    
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        print("mqttDidDisconnect: \(err?.localizedDescription ?? "")")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        print("didConnectAck: \(ack)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        print("didPublishMessage: \(message) and \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16) {
+        print("didReceiveMessage: \(message) and \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
+        print("didReceive trust")
+    }
 }
 
